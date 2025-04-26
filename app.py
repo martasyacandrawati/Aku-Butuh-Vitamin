1171ba22e592a59abe1ef75df162e939fc13cc19
martasyacandrawati <martasyacandrawati@gmail.com>
2025-04-26 12:04:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -20,5 +20,12 @@ with st.sidebar:
 │   └── page_2.py
 └── your_app.py
 
+import streamlit as st
+
+st.page_link("your_app.py", label="Home", icon="🏠")
+st.page_link("pages/page_1.py", label="Page 1", icon="1️⃣")
+st.page_link("pages/page_2.py", label="Page 2", icon="2️⃣", disabled=True)
+st.page_link("http://www.google.com", label="Google", icon="🌎")
+
 st.markdown("---")
 st.caption("📘 Made with Streamlit for educational purposes.")
