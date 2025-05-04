69b720baac2f20e5186b6f21a6d3f816b439f707
martasyacandrawati <martasyacandrawati@gmail.com>
2025-05-04 12:04:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,38 +6,47 @@ st.set_page_config(page_title="Buah dan Vitamin Didalam-nya", layout="centered")
 # Sidebar for input
 with st.sidebar:
     st.header("Menu")
-    selection = st.radio("Ingin Tahu Tentang Apa?", ["Kenali Vitamin", "Manfaat Vitamin", "Fungsi Vitamin", "Kekurangan", "Fakta Menarik","Tentang Kami"])
+    selection = st.radio("Ingin Tahu Tentang Apa?", ["Kenali Vitamin", "Manfaat Vitamin", "Fungsi Vitamin", "Kekurangan dan Kelebihan", "Fakta Menarik", "Tentang Kami"])
 
 # Konten berdasarkan pilihan
 if selection == "Kenali Vitamin":
     st.title("Yuk Kenali Macam-macam Vitamin!")
+    st.markdown("---")
     st.subheader("Vitamin Itu Apa?")
+    st.markdown("---")
     st.write("kita sudah tidak asing dengan vitamin, tetapi masih banyak orang yang jarang mengkonsumsi vitamin. Sebenarnya vitamin itu apa sih?")
     st.write("Vitamin adalah senyawa organik mikronutrien penting yang dibutuhkan tubuh dalam jumlah kecil untuk menjalankan berbagai fungsi biokimiawi. Vitamin tidak dapat disintesis oleh tubuh sendiri dan umumnya harus diperoleh dari makanan.") 
-
+    st.subheader("Apa Saja Macam Vitamin?")
+    st.markdown("---")
+    st.write("Vitamin dibagi menjadi dua kelompok utama yakni vitamin larut lemak (A, D, E, K) dan vitamin larut air (C dan B kompleks).")
 elif selection == "Manfaat Vitamin":
-    st.write("Ini konten untuk Mengetahui Manfaat Vitamin.")
+    st.title("Sebenarnya, Apa Saja Fungsi dan Sumber Dari Vitamin?")
+    st.write("Vitamin Memiliki Berbagai Fungsi Dari Berbagai Sumber loh!")
+    st.markdown("---")
+    st.subheader("Vitamin A")
+    st.markdown("---")
+    st.write("Vitamin Memiliki Berbagai Fungsi Dari Berbagai Sumber loh!")
+    st.subheader("Vitamin B")
+    st.markdown("---")
+    st.write("Vitamin Memiliki Berbagai Fungsi Dari Berbagai Sumber loh!")
+    st.subheader("Vitamin C")
+    st.markdown("---")
+    st.write("Vitamin Memiliki Berbagai Fungsi Dari Berbagai Sumber loh!")
+    st.subheader("Vitamin D")
+    st.markdown("---")
+    st.write("Vitamin Memiliki Berbagai Fungsi Dari Berbagai Sumber loh!")
+    st.subheader("Vitamin E")
+    st.markdown("---")
+    st.write("Vitamin Memiliki Berbagai Fungsi Dari Berbagai Sumber loh!")
+    st.subheader("Vitamin K")
+    st.markdown("---")
+    st.write("Vitamin Memiliki Berbagai Fungsi Dari Berbagai Sumber loh!")
 elif selection == "Fungsi Vitamin":
     st.write("Ini konten Mengetahui Fungsi Vitamin.")
-elif selection == "Kekurangan":
+elif selection == "Kekurangan dan Kelebihan":
     st.write("Ini konten untuk Mengetahui Kekurangan.")
 elif selection == "Fakta Menarik":
     st.write("Ini konten untuk Mengetahui Fakta Menarik Vitamin.")
-
-st.markdown(
-    """
-    <style>
-    body, h1, h2, h3, h4, h5, h6, p, span, label, div, li, ul, ol, select, input, textarea, button {
-        color: white !important;
-    }
-
-    .stApp {
-        background: linear-gradient(rgba(0, 0, 0, 0.85), rgba(0, 0, 0, 0.85)),
-                    url(https://www.freepik.com/free-photo/vitamin-b-tablets-yellow-background_1168207.htm#fromView=search&page=2&position=44&uuid=2bc5d22f-28ba-4221-b9a1-36be2ef3daa1&query=vitamin);
-        background-size: cover;
-        background-position: center;
-        background-repeat: no-repeat;
-    }
 
 st.markdown("---")
 st.caption("📘 Made with Streamlit for educational purposes.")
