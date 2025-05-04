b3b1f3d7a2452a68872fb7b61ea75adc6393475a
martasyacandrawati <martasyacandrawati@gmail.com>
2025-05-04 22:58:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -148,7 +148,7 @@ page_bg_style = """
 <style>
 /* Background Utama */
 .stApp {
-    background-image: url("https://www.freepik.com/premium-photo/high-angle-view-multi-colored-pencils-table_102573188.htm#fromView=search&page=1&position=38&uuid=869a1f72-6a5f-4dd6-97e6-e6dc73576937&query=medicine+pink");
+    background-image: url("https://www.freepik.com/free-photo/flat-lay-composition-medical-elements_12413015.htm#fromView=search&page=1&position=45&uuid=869a1f72-6a5f-4dd6-97e6-e6dc73576937&query=medicine+pink");
     background-size: cover;
     background-position: center;
     background-repeat: no-repeat;
