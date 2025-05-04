a7660904ad40d239ad7f90a0bb5eba4ff68dfd1b
martasyacandrawati <martasyacandrawati@gmail.com>
2025-05-04 22:55:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -148,7 +148,7 @@ page_bg_style = """
 <style>
 /* Background Utama */
 .stApp {
-    background-image: url("https://i.pinimg.com/736x/3c/d4/ed/3cd4ed56f9dac4e649f4eac8e62e7ebf.jpg");
+    background-image: url("https://i.pinimg.com/736x/b7/99/a1/b799a14446a6511b50f934abcb0eaf1c.jpg");
     background-size: cover;
     background-position: center;
     background-repeat: no-repeat;
