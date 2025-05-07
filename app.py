168552a35fe46de804fff073b72a582d0de3a00f
martasyacandrawati <martasyacandrawati@gmail.com>
2025-05-07 13:21:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -155,7 +155,10 @@ elif selection == "Kekurangan dan Kelebihan":
     st.write("Yuk kita lihat apa saja yang kita alami jika kelebihan vitamin.")
     st.write("Kalian tau ga sih? Mengonsumsi terlalu banyak vitamin, terutama yang larut dalam lemak, dapat menyebabkan keracunan dan masalah kesehatan lainnya pada dirimu loh!")
 elif selection == "Fakta Menarik":
-    st.write("Ini konten untuk Mengetahui Fakta Menarik Vitamin.")
+    st.header("Kalian tau ga sih vitamin juga punya fakta menarik loh! yuk, kita cari tau bersama!")
+    st.write("Vitamin tidak dapat dihasilkan oleh tubuh karena mereka adalah senyawa organik esensial yang dibutuhkan untuk berbagai fungsi tubuh, namun tubuh tidak memiliki kemampuan untuk memproduksinya secara alami.")
+    st.write("Vitamin yang dapat diproduksi oleh tubuh antara lain adalah vitamin D dan vitamin K. Tubuh dapat memproduksi vitamin D ketika kulit terpapar sinar matahari☀, dan vitamin K diproduksi oleh bakteri di usus besar🦠. Vitamin D berperan penting dalam kesehatan tulang dan sistem kekebalan tubuh🩻, sedangkan vitamin K berperan dalam pembekuan darah🧛🏻‍♀🩸.")
+    st.write("Jadi kesimpulannya tidak semua jenis vitamin dapat diproduksi secara alami oleh tubuh, sehingga penting bagi setiap individu untuk memenuhi asupan vitamin tubuh dengan mengonsumsi makanan sehat dan bergizi seimbang🥗🍉🥛")
 elif selection == "Quiz":
     st.title("Yuk, kita lihat apakah kamu sudah benar-benar mengetahui tentang vitamin🫵😏")
     st.markdown("---")
